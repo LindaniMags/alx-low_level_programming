f551431056e52bd96def90bb17d5ee423c81a6a3
lindaniMags <lindanimags@gmail.com>
2023-10-05 08:30:11 -0700

    commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,14 +6,18 @@
  *
  * Return: void
  */
-void reverse_array(int *a, int n) {
+void reverse_array(int *a, int n)
+{
     int start = 0;
     int end = n - 1;
     int temp;
-    while (start < end) {
+
+    while (start < end)
+    {
         temp = a[start];
         a[start] = a[end];
-        a[end] = temp; start++;
+        a[end] = temp;
+        start++;
         end--;
     }
 }
