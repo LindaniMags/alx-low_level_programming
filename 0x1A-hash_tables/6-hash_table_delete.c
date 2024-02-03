0fe5f24e35705c1e8aaac51df888cc2dfea27c03
LindaniMags <lindanimags@gmail.com>
2024-02-02 16:33:43 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,15 +9,15 @@ void hash_table_delete(hash_table_t *ht)
     hash_node_t *new_node, *pos;
     unsigned long int index;
 
-    if (ht == NULL)
+    if(ht == NULL)
         return;
 
-    for (index = 0; index < ht->size; index++)
+    for(index = 0; index < ht->size; index++)
     {
-        if (ht->array[index] != NULL)
+        if(ht->array[index] != NULL)
         {
             new_node = ht->array[index];
-            while (new_node != NULL)
+            while(new_node != NULL)
             {
                 pos = new_node->next;
                 free(new_node->key);
