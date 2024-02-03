13bf58bbf350a2fae58506354b15f6b023994be1
LindaniMags <lindanimags@gmail.com>
2024-02-02 17:50:26 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,39 +11,34 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *pos;
+	unsigned long int index;
+	hash_node_t *new_node, *pos;
 
-    if (!ht || !key || !*key || !value)
-        return (0);
-
-    index = key_index((const unsigned char *)key, ht->size);
-    pos = ht->array[index];
-
-    while (pos)
-    {
-        if (!strcmp(pos->key, key))
-        {
-            free(pos->value);
-            pos->value = strdup(value);
-            return (1);
-        }
-        pos = pos->next;
-    }
-
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
-
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    if (!new_node->key || !new_node->value)
-    {
-        free(new_node);
-        return (0);
-    }
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-    return (1);
+	if (!ht || !key || !*key || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	pos = ht->array[index];
+	while (pos)
+	{
+		if (!strcmp(pos->key, key))
+		{
+			free(pos->value);
+			pos->value = strdup(value);
+			return (1);
+		}
+		pos = pos->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (!new_node->key || !new_node->value)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
