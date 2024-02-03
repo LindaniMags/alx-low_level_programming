5f92ba332cf1264b4f1a38330afca9b73c308f83
LindaniMags <lindanimags@gmail.com>
2024-02-02 17:40:42 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,31 +9,30 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *new_node;
-    unsigned char position = 0;
-    unsigned long int index;
+	hash_node_t *new_node;
+	unsigned char position = 0;
+	unsigned long int index;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (index = 0; index < ht->size; index++)
-    {
-        if (ht->array[index] != NULL)
-        {
-            if (position == 1)
-                printf(", ");
-
-            new_node = ht->array[index];
-            while (new_node != NULL)
-            {
-                printf("'%s': '%s'", new_node->key, new_node->value);
-                new_node = new_node->next;
-                if (new_node != NULL)
-                    printf(", ");
-            }
-            position = 1;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			if (position == 1)
+				printf(", ");
+			new_node = ht->array[index];
+			while (new_node != NULL)
+			{
+				printf("'%s': '%s'", new_node->key, new_node->value);
+				new_node = new_node->next;
+				if (new_node != NULL)
+					printf(", ");
+			}
+			position = 1;
+		}
+	}
+	printf("}\n");
 }
