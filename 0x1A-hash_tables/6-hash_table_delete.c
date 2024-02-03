7f26cf9ac93749cac1c8f48e20144281e961467f
LindaniMags <lindanimags@gmail.com>
2024-02-02 16:30:09 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,28 +4,29 @@
  * hash_table_delete - Deletes a hash table.
  * @ht: Hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_table_t *head = ht;
-    hash_node_t *node, *pos;
+    hash_node_t *new_node, *pos;
     unsigned long int index;
+
+    if (ht == NULL)
+        return;
 
     for (index = 0; index < ht->size; index++)
     {
         if (ht->array[index] != NULL)
         {
-            node = ht->array[index];
-            while (node != NULL)
+            new_node = ht->array[index];
+            while (new_node != NULL)
             {
-                pos = node->next;
-                free(node->key);
-                free(node->value);
-                free(node);
-                node = pos;
+                pos = new_node->next;
+                free(new_node->key);
+                free(new_node->value);
+                free(new_node);
+                new_node = pos;
             }
         }
     }
-    free(head->array);
-    free(head);
+    free(ht->array);
+    free(ht);
 }
